a314a85b8bde9315f447b984266bb0b2dc4e5c90
John Nguyen <johnhoangn@gmail.com>
2021-06-16 01:26:11 -0400

    added UIButton

--- src/Client/Classes/UIButton.lua
+++ src/Client/Classes/UIButton.lua
@@ -1,10 +1,31 @@
+local Interface
 local UIButton = {}
 UIButton.__index = UIButton
 
 
-function UIButton.new(...)
-	
+
+function UIButton.new(element, container)
+    Interface = Interface or UIButton.Services.Interface
+
+	local self = setmetatable({
+        Instance = element;
+        Container = container;
+    }, UIButton)
+
+    self.MouseButton1Down = self.Instance.MouseButton1Down
+    self.MouseButton1Up = self.Instance.MouseButton1Up
+    self.MouseButton1Click = self.Instance.MouseButton1Click
+
+    self.MouseButton2Down = self.Instance.MouseButton2Down
+    self.MouseButton2Up = self.Instance.MouseButton2Up
+    self.MouseButton2Click = self.Instance.MouseButton2Click
+
+	return self
 end
 
+
+function UIButton:Destroy()
+    self.Instance:Destroy()
+end
 
 return UIButton
