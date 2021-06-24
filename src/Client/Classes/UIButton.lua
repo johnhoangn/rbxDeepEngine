9ac6ca2ebf656c3e9cfe7c7344dbfb13335ea215
John Nguyen <johnhoangn@gmail.com>
2021-06-24 04:02:37 -0400

    changed event bindings

--- src/Client/Classes/UIButton.lua
+++ src/Client/Classes/UIButton.lua
@@ -15,19 +15,77 @@ function UIButton.new(element, container)
         Container = container;
     }, UIButton)
 
-    self.MouseButton1Down = self.Instance.MouseButton1Down
-    self.MouseButton1Up = self.Instance.MouseButton1Up
-    self.MouseButton1Click = self.Instance.MouseButton1Click
-
-    self.MouseButton2Down = self.Instance.MouseButton2Down
-    self.MouseButton2Up = self.Instance.MouseButton2Up
-    self.MouseButton2Click = self.Instance.MouseButton2Click
+    self.Maid = self.Instancer:Make("Maid")
+    self:Bind()
 
 	return self
 end
 
 
+-- Binds events and gives them to a maid
+function UIButton:Bind()
+    self.MouseButton1Down = self.Instancer:Make("Signal")
+    self.MouseButton2Down = self.Instancer:Make("Signal")
+
+    self.MouseButton1Up = self.Instancer:Make("Signal")
+    self.MouseButton2Up = self.Instancer:Make("Signal")
+
+    self.MouseButton1Click = self.Instancer:Make("Signal")
+    self.MouseButton2Click = self.Instancer:Make("Signal")
+
+    self.Maid:GiveTask(self.MouseButton1Down)
+    self.Maid:GiveTask(self.MouseButton2Down)
+    self.Maid:GiveTask(self.MouseButton1Up)
+    self.Maid:GiveTask(self.MouseButton2Up)
+    self.Maid:GiveTask(self.MouseButton1Click)
+    self.Maid:GiveTask(self.MouseButton2Click)
+
+    self.Maid:GiveTask(self.Instance.UIButton.MouseButton1Down:Connect(function(...)
+        if (not Interface:Obscured(self)) then
+            self.MouseButton1Down:Fire(...)
+        end
+    end))
+
+    self.Maid:GiveTask(self.Instance.UIButton.MouseButton2Down:Connect(function(...)
+        if (not Interface:Obscured(self)) then
+            self.MouseButton2Down:Fire(...)
+        end
+    end))
+
+    self.Maid:GiveTask(self.Instance.UIButton.MouseButton1Up:Connect(function(...)
+        if (not Interface:Obscured(self)) then
+            self.MouseButton1Up:Fire(...)
+        end
+    end))
+
+    self.Maid:GiveTask(self.Instance.UIButton.MouseButton2Up:Connect(function(...)
+        if (not Interface:Obscured(self)) then
+            self.MouseButton2Up:Fire(...)
+        end
+    end))
+
+    self.Maid:GiveTask(self.Instance.UIButton.MouseButton1Click:Connect(function(...)
+        if (not Interface:Obscured(self)) then
+            self.MouseButton1Click:Fire(...)
+        end
+    end))
+
+    self.Maid:GiveTask(self.Instance.UIButton.MouseButton2Click:Connect(function(...)
+        if (not Interface:Obscured(self)) then
+            self.MouseButton2Click:Fire(...)
+        end
+    end))
+end
+
+
+-- Clears active event bindings
+function UIButton:Unbind()
+    self.Maid:DoCleaning()
+end
+
+
 function UIButton:Destroy()
+    self:Unbind()
     self.Instance:Destroy()
 end
 
