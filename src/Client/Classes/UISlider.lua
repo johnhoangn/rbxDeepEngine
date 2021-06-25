4c74b85a04343c94eb4812e99fc8def386610895
John Nguyen <johnhoangn@gmail.com>
2021-06-25 02:43:31 -0400

    mishandling button1up

--- src/Client/Classes/UISlider.lua
+++ src/Client/Classes/UISlider.lua
@@ -1,3 +1,9 @@
+-- UISlider Class, represents a Frame that acts as a slider
+-- Enduo(Dynamese)
+-- 6.24.2021
+
+
+
 local Interface
 local Mouse
 local UISlider = {}
@@ -9,8 +15,8 @@ UISlider.__index = UISlider
 -- @param slider <UISlider>
 -- @returns value between [0, 1] <Float>
 local function SliderValue(slider)
-    local width = slider.Instance.Clickbox.AbsoluteSize.X
-    local left = slider.Instance.Clickbox.AbsolutePosition.X
+    local width = slider.Instance.UISlider.AbsoluteSize.X
+    local left = slider.Instance.UISlider.AbsolutePosition.X
     local mousePos = Mouse.X
 
     return math.clamp((mousePos - left) / width, 0, 1)
@@ -19,25 +25,35 @@ end
 
 -- UISlider constructor
 -- @param element <Frame>
--- @param initialiValue == 0.5, <Float> [0, 1]
 -- @param container <UIContainer>
+-- @param initialiValue == 0.5, <Float> [0, 1]
 -- @return <UISlider>
-function UISlider.new(element, initialValue, container)
+function UISlider.new(element, container, initialValue)
     Interface = Interface or UISlider.Services.Interface
     Mouse = Mouse or UISlider.LocalPlayer:GetMouse()
 
 	local self = setmetatable({
         Instance = element;
-        Container = container;
+        _Container = container;
         Value = initialValue or 0.5;
     }, UISlider)
 
     self.Maid = self.Instancer:Make("Maid")
+    self.Sliding = self.Instancer:Make("Signal")
     self.Slid = self.Instancer:Make("Signal")
 
+    self.Maid:GiveTask(self.Sliding)
     self.Maid:GiveTask(self.Slid)
 
+    self:Bind()
+
 	return self
+end
+
+
+-- TODO: Move to UIElement superclass
+function UISlider:GetContainer()
+    return self._Container
 end
 
 
@@ -45,18 +61,19 @@ end
 function UISlider:Bind()
     self.Maid:GiveTask(
         self.Instance.UISlider.MouseButton1Down:Connect(function()
-            self.Sliding = Mouse.Move:Connect(function()
-                local ratio = SliderValue(self)
+            if (not Interface:Obscured(self)) then
+                self.Moving = Mouse.Move:Connect(function()
+                    local ratio = SliderValue(self)
 
-                self.Value = ratio
-                self.Slid:Fire(ratio)
-            end)
-        end)
-    )
+                    self.Value = ratio
+                    self.Sliding:Fire(ratio)
+                end)
 
-    self.Maid:GiveTask(
-        Mouse.Button1Up:Connect(function()
-            self.Sliding:Disconnect()
+                self.Release = Mouse.Button1Up:Connect(function()
+                    self.Moving:Disconnect()
+                    self.Slid:Fire(SliderValue(self))
+                end)
+            end
         end)
     )
 end
@@ -65,9 +82,11 @@ end
 -- Disconnects events
 function UISlider:Unbind()
     self.Maid:DoCleaning()
-    if (self.Sliding) then
-        self.Sliding:Disconnect()
-        self.Sliding = nil
+    if (self.Moving) then
+        self.Moving:Disconnect()
+        self.Moving = nil
+        self.Release:Disconnect()
+        self.Release = nil
     end
 end
 
